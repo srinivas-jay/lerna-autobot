251a339630851e29b8ebb247e230f013f750b164
Srinivas Jay <srini.j135@gmail.com>
2023-07-23 08:54:15 +1000

    initial commit for this repo with github actions core

---
+++ index.js
@@ -0,0 +1,12 @@
+const core = require('@actions/core');
+const github = require('@actions/github');
+
+async function run() {
+	try {
+		// TODO: Implement action logic
+	} catch (error) {
+		core.setFailed(error.message);
+	}
+}
+
+run();
